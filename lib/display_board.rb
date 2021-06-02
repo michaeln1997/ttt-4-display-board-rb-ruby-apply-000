deabf887c092b142ad110756569db4a2e1296a2f
Michael Nunes <michael.mundial@hotmail.com>
2021-06-02 18:39:06 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board1)
+  puts "#{board1[0]}|   |   "
+  puts "___________"
+  puts "#{board1[1]}| #{board1[2]} |   "
+  puts "___________" 
+  puts "#{board1[3]}| #{board1[4]} |   "
+end 
+  
+  
+board = ["X","X","O"," "," "," "," "," "," "]
+
+display_board(board)
